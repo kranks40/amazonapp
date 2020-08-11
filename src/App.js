6be719374a824f01ac46e0ba1b31fb92bd7213d9
kranks40 <eng_peters20@yahoo.com>
2020-08-11 06:10:44 -0400

    Added the home component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import {
   Route,
 } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
+      <div className='app'>
      <Switch>
-       <Route path='/checkout'><h1>Checkout</h1></Route>
-       <Route path='/login'><h1>Login Page</h1></Route>
+       <Route path='/checkout'>
+         <Header/>
+         <h1>Checkout</h1>
+         </Route>
+       <Route path='/login'>
+         <h1>Login Page</h1>
+         </Route>
        <Route path='/'>
          <Header/>
-         <h1>Home Page</h1></Route>
+         <Home/>
+         </Route>
      </Switch>
+     </div>
     </Router>    
   );
 }
